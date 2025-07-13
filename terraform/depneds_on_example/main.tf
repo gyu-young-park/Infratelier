719332af127fa6632e86bcf4e23d9b35f62da2cb
gyu-young-park <gyoue200125@gmail.com>
2025-07-13 23:34:55 +0900

    feat: add depends on example code

---
+++ terraform/depneds_on_example/main.tf
@@ -0,0 +1,35 @@
+data "aws_availability_zone" "first" {
+  state = "available"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+locals {
+  az = data.aws_availability_zone.first.names[0]
+}
+
+resource "aws_vpc" "this" {
+  cidr_block = var.vpc_cidr
+}
+
+resource "aws_subnet" "public" {
+  vpc_id = aws_vpc.this.id
+  cidr_block = "10.0.220.0/24"
+  availability_zone = local.az
+}
+
+resource "aws_internet_gateway" "this" {
+  
+}
+
+resource "aws_internet_gateway_attachment" "this" {
+  internet_gateway_id = aws_internet_gateway.this.id
+  vpc_id = aws_vpc.this.id
+}
+
+resource "aws_nat_gateway" "this" {
+  subnet_id = aws_subnet.public.id
+  depends_on = [ aws_internet_gateway.this ]
+}
